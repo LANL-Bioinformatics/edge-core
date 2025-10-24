450e79210a9c1a8076813180c93159a5310836cb
Yan Xu <yxu@pn1704854.lanl.gov>
2025-10-23 22:22:28 -0600

    Change home to wastewater

--- webapp/client/src/workflows/common/routes.js
+++ webapp/client/src/workflows/common/routes.js
@@ -1,3 +1,7 @@
-const workflowRoutes = []
+import React from 'react'
+
+const Home = React.lazy(() => import('src/workflows/wastewater/home'))
+
+const workflowRoutes = [{ path: '/home', name: 'Home', element: Home }]
 
 export default workflowRoutes
